cb7e49d51794918b2487946915c3898fa110674d
Sushil Dahal <sudahal@deerwalk.com>
2015-02-06 18:16:55 +0545

    Add Default Requestmap in Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,21 @@
+import com.bookstore.auth.Requestmap
+
 class BootStrap {
 
     def init = { servletContext ->
+        /*for (String url in [
+              '/', '/index', '/index.gsp', '*//**//*favicon.ico',
+              '/assets*//**', '*//**//*js*//**', '*//**//*css*//**', '*//**//*images*//**',
+              '/login', '/login.*', '/login*//*',
+              '/logout', '/logout.*', '/logout*//*']) {
+            new Requestmap(url: url, configAttribute: 'permitAll').save()
+        }
+        new Requestmap(url: '/profile*//**',    configAttribute: 'ROLE_USER').save()
+        new Requestmap(url: '/admin*//**',      configAttribute: 'ROLE_ADMIN').save()
+        new Requestmap(url: '/admin/role*//**', configAttribute: 'ROLE_SUPERVISOR').save()
+        new Requestmap(url: '/admin/user*//**', configAttribute: 'ROLE_ADMIN,ROLE_SUPERVISOR').save()
+        new Requestmap(url: '/j_spring_security_switch_user',
+              configAttribute: 'ROLE_SWITCH_USER,isFullyAuthenticated()').save()*/
     }
     def destroy = {
     }
