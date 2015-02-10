12aedc2e3104a5781df2822eaa07e96bf915543b
Sushil Dahal <sudahal@deerwalk.com>
2015-02-10 13:31:32 +0545

    security defined with static rules

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,22 +1,36 @@
-import com.bookstore.auth.Requestmap
+import com.bookstore.auth.Role
+import com.bookstore.auth.User
+import com.bookstore.auth.UserRole
 
 class BootStrap {
 
-    def init = { servletContext ->
-        /*for (String url in [
-              '/', '/index', '/index.gsp', '*//**//*favicon.ico',
-              '/assets*//**', '*//**//*js*//**', '*//**//*css*//**', '*//**//*images*//**',
-              '/login', '/login.*', '/login*//*',
-              '/logout', '/logout.*', '/logout*//*']) {
-            new Requestmap(url: url, configAttribute: 'permitAll').save()
-        }
-        new Requestmap(url: '/profile*//**',    configAttribute: 'ROLE_USER').save()
-        new Requestmap(url: '/admin*//**',      configAttribute: 'ROLE_ADMIN').save()
-        new Requestmap(url: '/admin/role*//**', configAttribute: 'ROLE_SUPERVISOR').save()
-        new Requestmap(url: '/admin/user*//**', configAttribute: 'ROLE_ADMIN,ROLE_SUPERVISOR').save()
-        new Requestmap(url: '/j_spring_security_switch_user',
-              configAttribute: 'ROLE_SWITCH_USER,isFullyAuthenticated()').save()*/
-    }
-    def destroy = {
-    }
+      def init = { servletContext ->
+//            for (String url in [
+//                  '/', '/index', '/index.gsp', '/**/favicon.ico',
+//                  '/assets/**', '/**/js/**', '/**/css/**', '/**/images/**',
+//                  '/login', '/login.*', '/login/*',
+//                  '/logout', '/logout.*', '/logout/*']) {
+//                  new Requestmap(url: url, configAttribute: 'permitAll').save()
+//            }
+//            new Requestmap(url: '/profile/**',    configAttribute: 'ROLE_USER').save()
+//            new Requestmap(url: '/admin/**',      configAttribute: 'ROLE_ADMIN').save()
+//            new Requestmap(url: '/admin/role/**', configAttribute: 'ROLE_SUPERVISOR').save()
+//            new Requestmap(url: '/admin/user/**', configAttribute: 'ROLE_ADMIN,ROLE_SUPERVISOR').save()
+//            new Requestmap(url: '/j_spring_security_switch_user',
+//                  configAttribute: 'ROLE_SWITCH_USER,isFullyAuthenticated()').save()
+
+            /*def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+            def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+            def testUser = new User(username: 'me', password: 'password')
+            testUser.save(flush: true)
+
+            UserRole.create testUser, adminRole, true
+
+            assert User.count() == 1
+            assert Role.count() == 2
+            assert UserRole.count() == 1*/
+      }
+      def destroy = {
+      }
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -16,19 +16,19 @@ grails.project.groupId = appName // change this to alter the default package nam
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
 grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
 grails.mime.types = [ // the first one is the default format
-    all:           '*/*', // 'all' maps to '*' or the first available format in withFormat
-    atom:          'application/atom+xml',
-    css:           'text/css',
-    csv:           'text/csv',
-    form:          'application/x-www-form-urlencoded',
-    html:          ['text/html','application/xhtml+xml'],
-    js:            'text/javascript',
-    json:          ['application/json', 'text/json'],
-    multipartForm: 'multipart/form-data',
-    rss:           'application/rss+xml',
-    text:          'text/plain',
-    hal:           ['application/hal+json','application/hal+xml'],
-    xml:           ['text/xml', 'application/xml']
+                      all:           '*/*', // 'all' maps to '*' or the first available format in withFormat
+                      atom:          'application/atom+xml',
+                      css:           'text/css',
+                      csv:           'text/csv',
+                      form:          'application/x-www-form-urlencoded',
+                      html:          ['text/html','application/xhtml+xml'],
+                      js:            'text/javascript',
+                      json:          ['application/json', 'text/json'],
+                      multipartForm: 'multipart/form-data',
+                      rss:           'application/rss+xml',
+                      text:          'text/plain',
+                      hal:           ['application/hal+json','application/hal+xml'],
+                      xml:           ['text/xml', 'application/xml']
 ]
 
 // URL Mapping Cache Max Size, defaults to 5000
@@ -43,20 +43,20 @@ grails.controllers.defaultScope = 'singleton'
 
 // GSP settings
 grails {
-    views {
-        gsp {
-            encoding = 'UTF-8'
-            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
-            codecs {
-                expression = 'html' // escapes values inside ${}
-                scriptlet = 'html' // escapes output from scriptlets in GSPs
-                taglib = 'none' // escapes output from taglibs
-                staticparts = 'none' // escapes output from static template parts
+      views {
+            gsp {
+                  encoding = 'UTF-8'
+                  htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
+                  codecs {
+                        expression = 'html' // escapes values inside ${}
+                        scriptlet = 'html' // escapes output from scriptlets in GSPs
+                        taglib = 'none' // escapes output from taglibs
+                        staticparts = 'none' // escapes output from static template parts
+                  }
             }
-        }
-        // escapes all not-encoded output at final stage of outputting
-        // filteringCodecForContentType.'text/html' = 'html'
-    }
+            // escapes all not-encoded output at final stage of outputting
+            // filteringCodecForContentType.'text/html' = 'html'
+      }
 }
 
 
@@ -86,43 +86,44 @@ grails.hibernate.pass.readonly = false
 grails.hibernate.osiv.readonly = false
 
 environments {
-    development {
-        grails.logging.jul.usebridge = true
-    }
-    production {
-        grails.logging.jul.usebridge = false
-        // TODO: grails.serverURL = "http://www.changeme.com"
-    }
+      development {
+            grails.logging.jul.usebridge = true
+      }
+      production {
+            grails.logging.jul.usebridge = false
+            // TODO: grails.serverURL = "http://www.changeme.com"
+      }
 }
 
 // log4j configuration
 log4j.main = {
-    // Example of changing the log pattern for the default console appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+      // Example of changing the log pattern for the default console appender:
+      //
+      //appenders {
+      //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+      //}
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+      debug 'org.springframework.security'
+
+      error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+            'org.codehaus.groovy.grails.web.pages',          // GSP
+            'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+            'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+            'org.codehaus.groovy.grails.commons',            // core / classloading
+            'org.codehaus.groovy.grails.plugins',            // plugins
+            'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+            'org.springframework',
+            'org.hibernate',
+            'net.sf.ehcache.hibernate'
 }
+
 
 
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.bookstore.auth.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.bookstore.auth.UserRole'
 grails.plugin.springsecurity.authority.className = 'com.bookstore.auth.Role'
-grails.plugin.springsecurity.requestMap.className = 'com.bookstore.auth.Requestmap'
-grails.plugin.springsecurity.securityConfigType = 'Requestmap'
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	'/':                              ['permitAll'],
 	'/index':                         ['permitAll'],
